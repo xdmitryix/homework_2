2862a8ba3194f912fb607e27272c56114084ef8d
dmitry <rbdeman@yandex.ru>
2022-11-25 11:46:11 +0300

    сделал третью задачу

--- Zadacha_three/Program.cs
+++ Zadacha_three/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("введи номер дня недели: ");
+int number = int.Parse(Console.ReadLine());
+if ((number < 1) ^ (number > 7)) //добавил проверку существования дня недели
+{
+    Console.WriteLine("нет такого дня недели");
+}
+else
+{
+    if ((number >= 1) & (number <= 5))
+    {
+        Console.WriteLine("нет");
+    }
+    else
+    {
+        Console.WriteLine("да");
+    }
+}
